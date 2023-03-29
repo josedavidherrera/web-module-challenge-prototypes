9b4bf5c89e63f81d95d66601d42c0b55b7142883
Jose Herrera <joseherrera@Joses-Air.home>
2023-03-28 20:44:15 -0700

    done

--- index.js
+++ index.js
@@ -15,9 +15,29 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+  }
+  Person.prototype.poop = function(){
+    this.stomach = []
+  }
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`
+  }
+  const mary = new Person('Mary', 50)
+  console.log(mary.toString());
+  mary.eat('pizza');
+  console.log(mary.stomach);
+  mary.poop();
+  console.log(mary.stomach)
+
 
 
 /*
@@ -36,8 +56,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, mpg) {
+  this.model = model
+  this.milesPerGallon = mpg
+  this.tank = 0
+  this.odometer = 0
+}
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons
+}
+Car.prototype.drive = function(distance){
+  const drivableMiles = this.tank * this.milesPerGallon;
+  if (dist <= drivableMiles){
+    this.odometer = this.odometer + distance
+    this.tank = this.tank - (distance / this.milesPerGallon)
+  }else{
+    this.odometer = this.odometer + drivableMiles;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
 }
 
 
@@ -49,9 +86,15 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
 }
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
+
 
 
 /* 
